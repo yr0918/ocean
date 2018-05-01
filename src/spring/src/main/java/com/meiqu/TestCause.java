8807ebd6c61831731ef45d36f7e5e76634af776f
yr0918@hotmail.com <yheDev2015>
2018-05-02 07:17:10 +0800

    update

---
+++ src/spring/src/main/java/com/meiqu/TestCause.java
@@ -0,0 +1,13 @@
+package com.meiqu.resume.down.resource;
+
+/**
+ * @author yerong
+ * @version 1.0
+ * @since 1.0
+ * 创建于17/12/13
+ */
+public class TestCause {
+    public String result() {
+        return "hello";
+    }
+}
---
+++ src/spring/src/main/java/com/meiqu/TestResources.java
@@ -0,0 +1,17 @@
+package com.meiqu.resume.down.resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestResources {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestResources.class);
+
+    @RequestMapping(value = "refresh", method = RequestMethod.GET)
+    public String refresh() {
+        return new TestCause().result();
+    }
+}
